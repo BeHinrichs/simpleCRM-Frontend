7285eeaa40b371cafe6855c48ef515457ec5f7a6
Ben <benjamin.hinrichs@gmx.net>
2025-08-15 09:44:46 +0200

    FilterLogik funktioniert. Fehlt noch Update Task

--- src/app/dashboard/task/services/task-service.ts
+++ src/app/dashboard/task/services/task-service.ts
@@ -1,9 +1,9 @@
 import { Injectable, signal, inject } from '@angular/core';
 import { HttpClient, HttpParams} from '@angular/common/http';
 import { tasks } from '../data/mock-data';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject, combineLatest } from 'rxjs';
 import { TaskInterface } from '../../models/task-interface';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap, tap, map, shareReplay } from 'rxjs/operators';
 
 type FilterValue = 'own' | 'all' | 'done';
 
@@ -21,24 +21,33 @@ export class TaskService {
   tasks = signal<TaskInterface[]>([]);
   HttpClient = inject(HttpClient)
   private readonly baseUrl = 'http://127.0.0.1:8000/api/tasks/';
+  private allTasks$ = this.HttpClient.get<TaskInterface[]>(this.baseUrl).pipe(
+    shareReplay(1)
+  );
   private filter$ = new BehaviorSubject<FilterValue>('own');
 
   constructor() { }
 
-  public tasks$: Observable<TaskInterface[]> = this.filter$.pipe(
-    switchMap(filter => {
-      console.log(`Filter geändert auf: ${filter}, frage neue Daten an...`);
-      if (filter === 'all') {
-        return this.HttpClient.get<TaskInterface[]>(this.baseUrl);
+  public tasks$: Observable<TaskInterface[]> = combineLatest([
+    this.allTasks$,
+    this.filter$
+  ]).pipe(
+    // 4. Die map-Operation führt die eigentliche Filterung im Frontend durch.
+    map(([tasks, filter]) => {
+      /* console.log(`Filtere ${tasks.length} Tasks mit dem Filter: '${filter}'`); */
+      const currentUser = "Ben"; // Später durch echten User ersetzen
+
+      switch (filter) {
+        case 'own':
+          console.log('Alle Tasks: ', this.HttpClient.get<TaskInterface[]>(this.baseUrl))
+          return tasks.filter((t: { name: string; status: string; }) => t.name === currentUser && t.status === 'incomplete');
+        case 'all':
+          return tasks.filter((t: { status: string; }) => t.status === 'incomplete');
+        case 'done':
+          return tasks.filter((t: { name: string; status: string; }) => t.status === 'complete');
+        default:
+          return tasks;
       }
-      
-      let params = new HttpParams();
-      if (filter === 'own') {
-        params = params.set('name', 'Ben').set('status', 'incomplete');
-      } else if (filter === 'done') {
-        params = params.set('status', 'complete');
-      }
-      return this.HttpClient.get<TaskInterface[]>(this.baseUrl, { params });
     })
   );
 
